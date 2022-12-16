c9c98b373517bdf6f7ad671238a042e9c1f58931
hproptech-vietpham <viet.phh@hproptech.vn>
2022-12-16 17:49:26 +0700

    chore: fix fail

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
-import CatCard from '../components/templates/cards/cat/CatCard';
-import { mockCatCardProps } from '../components/templates/cards/cat/CatCard.mocks';
 import styles from '../styles/Home.module.css';
 import { NextPageWithLayout } from './page';
 
@@ -11,7 +9,6 @@ const Home: NextPageWithLayout = () => {
       <h1 className={styles.title}>
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
-      <CatCard {...mockCatCardProps.base} />
     </section>
   );
 };
