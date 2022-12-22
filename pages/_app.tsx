9b3637c24c0ecfd9e3bf77f35ff0292d7ce7de4c
letuthptnguyendu <letuthptnguyendueduvn@gmail.com>
2022-12-22 17:57:02 +0700

    feat: add config provider for antd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles/style.scss';
@@ -13,7 +14,15 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
       <Head>
         <title>Nhà thuốc Phước Thiện</title>
       </Head>
-      {getLayout(<Component {...pageProps} />)}{' '}
+      <ConfigProvider
+        theme={{
+          token: {
+            fontFamily: 'Roboto',
+          },
+        }}
+      >
+        {getLayout(<Component {...pageProps} />)}{' '}
+      </ConfigProvider>
     </>
   );
 }
