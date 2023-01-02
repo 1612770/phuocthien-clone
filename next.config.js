ae8a44d575ed9634fde5c841771a98a06ef6e7ab
hproptech-vietpham <viet.phh@hproptech.vn>
2023-01-02 10:59:44 +0700

    build: dockerfile

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
