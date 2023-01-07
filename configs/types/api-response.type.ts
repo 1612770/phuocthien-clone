7e1359cf69ccf48920d243ce53628dc2927b6e3f
letuthptnguyendu <letuthptnguyendueduvn@gmail.com>
2023-01-07 17:11:27 +0700

    feat: api response model

--- configs/types/api-response.type.ts
+++ configs/types/api-response.type.ts
@@ -1,10 +1,12 @@
 type APIResponse<T = any> = {
-  data: T;
+  data?: T;
   error: Partial<{
     code: string;
     msg: string;
   }> | null;
   success: boolean;
+  statusCode?: number;
+  message?: string;
 };
 
 export default APIResponse;
