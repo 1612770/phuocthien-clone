70fc7bb9a35c7db031099ed93b6683cbaf91300b
letuthptnguyendu <letuthptnguyendueduvn@gmail.com>
2023-01-14 10:36:38 +0700

    chore: disable strict mode

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-
-  reactStrictMode: true,
+  reactStrictMode: false,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
