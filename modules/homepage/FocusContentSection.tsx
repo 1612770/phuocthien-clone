34148253623733a33e8372269d5fccc20dbd27fe
letuthptnguyendu <letuthptnguyendueduvn@gmail.com>
2023-02-23 18:35:32 +0700

    update: focus content change UI

--- modules/homepage/FocusContentSection.tsx
+++ modules/homepage/FocusContentSection.tsx
@@ -10,35 +10,42 @@ function FocusContentSection({
   focusContent: FocusContentModel[];
 }) {
   return (
-    <div className="mt-[32px] p-2 lg:container lg:mt-[40px] lg:p-0">
-      <Typography.Title level={2} className="text-center font-medium ">
-        Mua thuốc dễ dàng tại Phước Thiện
-      </Typography.Title>
+    <div className=" mt-[32px] bg-primary-background py-4">
+      <div className=" p-2 lg:container lg:p-0">
+        <Typography.Title
+          level={2}
+          className="mb-4 mt-4 text-center text-2xl font-medium uppercase lg:hidden lg:text-left"
+        >
+          Mua thuốc dễ dàng tại Phước Thiện
+        </Typography.Title>
 
-      <div className="mt-4 grid grid-cols-1 gap-2  md:grid-cols-2 md:gap-4 lg:grid-cols-4 ">
-        {focusContent?.map((focus, index) => (
-          <LinkWrapper href={focus.url} key={index}>
-            <div className="flex h-full flex-col items-center rounded-lg border border-solid border-gray-300 p-4">
-              <div className="relative h-[40px] w-[40px] lg:h-[80px] lg:w-[80px]">
-                <ImageWithFallback
-                  width={80}
-                  height={80}
-                  layout="fill"
-                  src={focus.imageUrl || ''}
-                  getMockImage={() => ImageUtils.getFocusMockUrl()}
-                ></ImageWithFallback>
+        <div className="my-4 grid grid-cols-1 gap-2  md:grid-cols-2 md:gap-4 lg:grid-cols-4 ">
+          {focusContent?.map((focus, index) => (
+            <LinkWrapper href={focus.url} key={index}>
+              <div className="flex h-full flex-col items-center rounded-lg ">
+                <div className="relative mt-3 h-[40px] w-[40px] min-w-[40px]">
+                  <ImageWithFallback
+                    width={80}
+                    height={80}
+                    layout="fill"
+                    src={focus.imageUrl || ''}
+                    getMockImage={() => ImageUtils.getFocusMockUrl()}
+                  ></ImageWithFallback>
+                </div>
+
+                <div className="ml-2 flex flex-col">
+                  <Typography.Text className="mt-2 text-center text-lg font-medium text-primary ">
+                    {focus.name}
+                  </Typography.Text>
+
+                  <Typography.Text className=" px-4 text-center ">
+                    {focus.description}
+                  </Typography.Text>
+                </div>
               </div>
-
-              <Typography.Text className="mt-2 text-center text-lg font-medium text-primary">
-                {focus.name}
-              </Typography.Text>
-
-              <Typography.Text className="text-center">
-                {focus.description}
-              </Typography.Text>
-            </div>
-          </LinkWrapper>
-        ))}
+            </LinkWrapper>
+          ))}
+        </div>
       </div>
     </div>
   );
