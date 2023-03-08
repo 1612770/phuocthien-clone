fa6b888b3796ff82681d387ee70f4879ef0b22e7
letuthptnguyendu <letuthptnguyendueduvn@gmail.com>
2023-03-08 21:57:53 +0700

    update: remove mock images

--- libs/utils/image.utils.ts
+++ libs/utils/image.utils.ts
@@ -1,58 +1,19 @@
 class ImageUtils {
-  static IMAGE_HOST = 'https://phuocthien.net';
-  static MENU_MOCKS = [
-    '/mock/menu-mock-1.png',
-    '/mock/menu-mock-2.png',
-    '/mock/menu-mock-3.png',
-    '/mock/menu-mock-4.png',
-    '/mock/menu-mock-5.png',
-    '/mock/menu-mock-6.png',
-    '/mock/menu-mock-7.png',
-  ];
-  static PRODUCT_MOCKS = [
-    '/mock/product-mock-1.jpg',
-    '/mock/product-mock-2.jpg',
-    '/mock/product-mock-3.jpg',
-    '/mock/product-mock-4.jpg',
-    '/mock/product-mock-5.jpg',
-    '/mock/product-mock-6.jpg',
-    '/mock/product-mock-7.jpg',
-    '/mock/product-mock-8.jpg',
-    '/mock/product-mock-9.jpg',
-    '/mock/product-mock-10.jpg',
-  ];
+  static IMAGE_HOST = process.env.HOST_IMAGE?.includes('http')
+    ? process.env.HOST_IMAGE
+    : `https://${process.env.HOST_IMAGE}`;
 
-  static CAPAIGN_MOCKS = [
-    '/mock/campaign-mock-1.png',
-    '/mock/campaign-mock-2.jpeg',
-    '/mock/campaign-mock-3.jpeg',
-    '/mock/campaign-mock-4.png',
-  ];
+  static MENU_MOCKS = ['/image-placeholder.png'];
 
-  static CHECKOUT_MOCKS = [
-    '/mock/checkout-1.png',
-    '/mock/checkout-2.png',
-    '/mock/checkout-3.png',
-    '/mock/checkout-4.png',
-    '/mock/checkout-5.png',
-  ];
+  static PRODUCT_MOCKS = ['/image-placeholder.png'];
 
-  static DRUGSTORE_MOCKS = [
-    '/mock/drug-store-1.png',
-    '/mock/drug-store-2.png',
-    '/mock/drug-store-3.png',
-    '/mock/drug-store-4.png',
-    '/mock/drug-store-5.png',
-  ];
+  static CAPAIGN_MOCKS = ['/image-placeholder.png'];
 
-  static FOCUS_MOCKS = [
-    '/mock/focus-mock-1.png',
-    '/mock/focus-mock-2.png',
-    '/mock/focus-mock-3.png',
-    '/mock/focus-mock-4.png',
-    '/mock/focus-mock-5.png',
-    '/mock/focus-mock-6.png',
-  ];
+  static CHECKOUT_MOCKS = ['/image-placeholder.png'];
+
+  static DRUGSTORE_MOCKS = ['/image-placeholder.png'];
+
+  static FOCUS_MOCKS = ['/image-placeholder.png'];
 
   static getFullImageUrl(path?: string) {
     if (!path) return '';
