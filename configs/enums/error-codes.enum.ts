36caab6cc9bc3d89f61f2f40850c2f983635e685
letuthptnguyendu <letuthptnguyendueduvn@gmail.com>
2023-03-08 21:56:46 +0700

    fix: catch user unknow

--- configs/enums/error-codes.enum.ts
+++ configs/enums/error-codes.enum.ts
@@ -1,5 +1,6 @@
 enum ErrorCodes {
   PHONE_EXISTED_IN_SYSTEM = 'PHONE_EXISTED_IN_SYSTEM',
+  USER_NOT_FOUND = 'USER_NOT_FOUND',
 }
 
 export default ErrorCodes;
--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -5,6 +5,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useAppMessage } from './AppMessageProvider';
 import { useRouter } from 'next/router';
 import { useAppConfirmDialog } from './AppConfirmDialogProvider';
+import ErrorCodes from '@configs/enums/error-codes.enum';
 
 const AuthContext = React.createContext<{
   userData?: ProfileModel;
@@ -35,7 +36,7 @@ function AuthProvider({ children }: { children: React.ReactNode }) {
         router.replace('/dang-nhap');
       },
     });
-  }, [router]);
+  }, [router, setConfirmData]);
 
   const getAuthProfile = async () => {
     try {
@@ -44,7 +45,13 @@ function AuthProvider({ children }: { children: React.ReactNode }) {
       if (profileResult.data) {
         setUserData(profileResult.data);
       }
-    } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      if (error?.error?.code === ErrorCodes.USER_NOT_FOUND) {
+        setUserData(undefined);
+        setCookie(null, '', COOKIE_KEYS.TOKEN);
+      }
+
       toastError({ data: error });
     }
   };
@@ -55,6 +62,7 @@ function AuthProvider({ children }: { children: React.ReactNode }) {
     if (token) {
       getAuthProfile();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const isUserLoggedIn = !!userData?.id;
