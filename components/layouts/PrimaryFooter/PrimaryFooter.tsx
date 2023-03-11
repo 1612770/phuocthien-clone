ce3af1a6ea8d583dd5985dd2c2e61a55523855ab
letuthptnguyendu <letuthptnguyendueduvn@gmail.com>
2023-03-11 17:54:40 +0700

    update: footer change layout and styles

--- components/layouts/PrimaryFooter/PrimaryFooter.tsx
+++ components/layouts/PrimaryFooter/PrimaryFooter.tsx
@@ -17,9 +17,30 @@ function PrimaryFooter() {
       </div>
       <footer className="bg-primary px-4">
         <div className="py-8 lg:container">
-          <div className="grid grid-cols-1 items-end gap-4 md:grid-cols-2 lg:grid-cols-[200px,_1fr,_400px]">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
             <div>
-              <img src={IMAGES.logo} alt="logo" className=" block h-10" />
+              <Link href="/" style={{ color: 'white' }}>
+                <a className="flex items-center">
+                  <img
+                    src={IMAGES.logo}
+                    alt="Nhà thuốc Phước Thiện"
+                    className="mr-2 h-8 object-contain"
+                  />
+
+                  <Space
+                    direction="vertical"
+                    size={0}
+                    className="mr-4 w-[92px]"
+                  >
+                    <Typography.Text className="m-0 -mb-2 inline-block text-base text-white">
+                      Nhà thuốc
+                    </Typography.Text>
+                    <Typography.Text strong className="uppercase text-white">
+                      Phước Thiện
+                    </Typography.Text>
+                  </Space>
+                </a>
+              </Link>
               <Space className="mt-2">
                 <Link
                   href="https://www.facebook.com/PhuocThienPharmacy/"
@@ -49,14 +70,22 @@ function PrimaryFooter() {
             </div>
 
             <div>
-              <Typography.Text className=" uppercase text-white">
-                Tải về ứng dụng Phước Thiện
+              <Typography.Text className=" font-semibold uppercase text-white">
+                Tải ứng dụng
               </Typography.Text>
-              <div className="mt-2 flex gap-2 lg:gap-4">
-                <a href="https://play.google.com/store/apps/details?id=com.esuspharmacy.phuocthien&hl=en">
+              <div className="mt-2 flex gap-1 lg:gap-2">
+                <a
+                  href="https://play.google.com/store/apps/details?id=com.esuspharmacy.phuocthien&hl=en"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <img src="/chplay.png" alt="" className="w-[120px]" />
                 </a>
-                <a href="https://apps.apple.com/us/app/nh%C3%A0-thu%E1%BB%91c-ph%C6%B0%E1%BB%9Bc-thi%E1%BB%87n/id1662328703">
+                <a
+                  target="_blank"
+                  href="https://apps.apple.com/us/app/nh%C3%A0-thu%E1%BB%91c-ph%C6%B0%E1%BB%9Bc-thi%E1%BB%87n/id1662328703"
+                  rel="noreferrer"
+                >
                   <img src="/appstore.png" alt="" className="w-[120px]" />
                 </a>
               </div>
@@ -89,7 +118,7 @@ function PrimaryFooter() {
           <Divider className="bg-white" />
           <div>
             <Typography className="text-center text-xs text-white">
-              © 2020 HỆ THỐNG NHÀ THUỐC PHƯỚC THIỆN. Giấy phép kinh doanh:
+              © 2023 HỆ THỐNG NHÀ THUỐC PHƯỚC THIỆN. Giấy phép kinh doanh:
               32A8031215 - được cấp ngày: 16/04/2020. Địa chỉ: 370 Trưng Nữ
               Vương, Quận Hải Châu, TP Đà Nẵng. Điện thoại: (0236) 3827.772 -
               Fax: (0236) 3827.772. Email: phuocthien.pharmacy@gmail.com. Người
@@ -100,11 +129,17 @@ function PrimaryFooter() {
             </Typography>
           </div>
           <div className="mt-4 flex items-center justify-center">
-            <img
-              src={IMAGES.bocongthuongDathongbao}
-              alt="bocongthuong-dathongbao"
-              className="h-10"
-            />
+            <a
+              href="http://online.gov.vn/(X(1)S(m3qt0ctfachyjjpv1g2sbk23))/Home/WebDetails/53823?AspxAutoDetectCookieSupport=1"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={IMAGES.bocongthuongDathongbao}
+                alt="bocongthuong-dathongbao"
+                className="h-10"
+              />
+            </a>
             <img
               src={IMAGES.dmcaProtected}
               alt="dmca-protected"
