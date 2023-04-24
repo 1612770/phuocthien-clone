eacee3f175c887173129e95abcc990f62593d560
letuthptnguyendu <letuthptnguyendueduvn@gmail.com>
2023-04-24 16:38:32 +0700

    update: optimize product page

--- pages/san-pham/chi-tiet/[product]/index.tsx
+++ pages/san-pham/chi-tiet/[product]/index.tsx
@@ -25,6 +25,7 @@ import PriceUnit from '@modules/products/PriceUnit';
 import { Promotion } from '@configs/models/promotion.model';
 import { useCart } from '@providers/CartProvider';
 import { InfoCircleOutlined } from '@ant-design/icons';
+import ProductMetaData from '@modules/products/ProductMetaData';
 
 const getMaxDiscount = (promotions: Promotion[]): number => {
   let maxDiscount = 0;
@@ -195,111 +196,16 @@ const ProductPage: NextPageWithLayout<{
                   </div>
                 )}
               </div>
-              <PromotionList
-                promotions={product.promotions || []}
-                retailPrice={product.retailPrice}
-              />
+              {!!product.promotions?.length && (
+                <PromotionList
+                  promotions={product.promotions || []}
+                  retailPrice={product.retailPrice}
+                />
+              )}
             </div>
           </div>
 
-          <div className="mb-2 mt-8">
-            {!!product?.productionBrand?.name && (
-              <div className="my-2 grid grid-cols-1 md:grid-cols-[120px,_1fr]">
-                <Typography.Text className=" font-medium ">
-                  Thương hiệu
-                </Typography.Text>
-                <Typography.Text className=" ml-0 md:ml-2">
-                  {product?.productionBrand?.name}
-                </Typography.Text>
-              </div>
-            )}
-            {!!product?.registrationNumber && (
-              <div className="my-2 grid grid-cols-1 md:grid-cols-[120px,_1fr]">
-                <Typography.Text className=" font-medium ">
-                  Số đăng ký
-                </Typography.Text>
-                <Typography.Text className=" ml-0 md:ml-2">
-                  {product?.registrationNumber}
-                </Typography.Text>
-              </div>
-            )}
-            {!!product?.packagingProcess && (
-              <div className="my-2 grid grid-cols-1 md:grid-cols-[120px,_1fr]">
-                <Typography.Text className=" font-medium ">
-                  Quy cách đóng gói
-                </Typography.Text>
-                <Typography.Text className=" ml-0 md:ml-2">
-                  {product?.packagingProcess}{' '}
-                  {product?.detail?.packedType
-                    ? `(${product?.detail?.packedType})`
-                    : ``}
-                </Typography.Text>
-              </div>
-            )}
-
-            {!!product?.detail?.drugUsers && (
-              <div className="my-2 grid grid-cols-1 md:grid-cols-[120px,_1fr]">
-                <Typography.Text className=" font-medium ">
-                  Đối tượng sử dụng
-                </Typography.Text>
-                <Typography.Text className=" ml-0 md:ml-2">
-                  {product?.detail?.drugUsers}
-                </Typography.Text>
-              </div>
-            )}
-            {!!product?.ingredient && (
-              <div className="my-2 grid grid-cols-1 md:grid-cols-[120px,_1fr]">
-                <Typography.Text className=" whitespace-nowrap font-medium">
-                  Hoạt chất
-                </Typography.Text>
-                <Typography.Text className=" ml-0 md:ml-2">
-                  {product?.ingredient}
-                </Typography.Text>
-              </div>
-            )}
-
-            {!!product?.drugContent && (
-              <div className="my-2 grid grid-cols-1 md:grid-cols-[120px,_1fr]">
-                <Typography.Text className=" font-medium ">
-                  Hàm lượng
-                </Typography.Text>
-                <Typography.Text className=" ml-0 md:ml-2">
-                  {product?.drugContent}
-                </Typography.Text>
-              </div>
-            )}
-
-            <div className="my-2 grid grid-cols-1 md:grid-cols-[120px,_1fr]">
-              <Typography.Text className=" font-medium ">
-                Là thuốc kê đơn
-              </Typography.Text>
-              <Typography.Text className=" ml-0 md:ml-2">
-                {product?.isPrescripted ? 'Có' : 'Không'}
-              </Typography.Text>
-            </div>
-
-            {product?.isSpecial && (
-              <div className="my-2 grid grid-cols-1 md:grid-cols-[120px,_1fr]">
-                <Typography.Text className=" font-medium ">
-                  Là thuốc đặc biệt
-                </Typography.Text>
-                <Typography.Text className=" ml-0 md:ml-2">
-                  {product?.isSpecial ? 'Có' : 'Không'}
-                </Typography.Text>
-              </div>
-            )}
-
-            {product?.isMental && (
-              <div className="my-2 grid grid-cols-1 md:grid-cols-[120px,_1fr]">
-                <Typography.Text className=" font-medium ">
-                  Là thuốc tâm thần
-                </Typography.Text>
-                <Typography.Text className=" ml-0 md:ml-2">
-                  {product?.isMental ? 'Có' : 'Không'}
-                </Typography.Text>
-              </div>
-            )}
-          </div>
+          {product && <ProductMetaData product={product} />}
         </div>
 
         <div className="w-full ">
@@ -418,10 +324,13 @@ export const getServerSideProps = async (
 
   const productClient = new ProductClient(context, {});
   const offerClient = new OfferClient(context, {});
+  const productKey = UrlUtils.getKeyFromParam(
+    context.params?.product as string
+  );
 
   try {
     const product = await productClient.getProduct({
-      key: UrlUtils.getKeyFromParam(context.params?.product as string),
+      key: productKey,
     });
 
     if (product.data) {
@@ -431,12 +340,8 @@ export const getServerSideProps = async (
         productClient.getProducts({
           page: 1,
           pageSize: 10,
-          productTypeKey: UrlUtils.getKeyFromParam(
-            product.data.productType?.key
-          ),
-          productGroupKey: UrlUtils.getKeyFromParam(
-            product.data.productGroup?.key
-          ),
+          productTypeKey: product.data.productType?.key,
+          productGroupKey: product.data.productGroup?.key,
           isPrescripted: false,
         }),
         offerClient.getAllActiveOffers(),
@@ -449,12 +354,14 @@ export const getServerSideProps = async (
       }
 
       if (products.data) {
-        serverSideProps.props.otherProducts = products.data.data.slice(0, 4);
+        serverSideProps.props.otherProducts = products.data.data
+          .filter((product) => product.key !== productKey)
+          .slice(0, 4);
       }
     }
 
     const drugStoresAvailable = await productClient.checkInventoryAtDrugStores({
-      key: UrlUtils.getKeyFromParam(context.params?.product as string),
+      key: productKey,
     });
 
     if (drugStoresAvailable.data?.length) {
