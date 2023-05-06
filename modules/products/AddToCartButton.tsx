965b06a4330d19f2f27e5dbcace3ac90ad68eb78
letuthptnguyendu <letuthptnguyendueduvn@gmail.com>
2023-05-06 20:42:40 +0700

    update: save product quantity into ref, ask when remove

--- modules/products/AddToCartButton.tsx
+++ modules/products/AddToCartButton.tsx
@@ -1,9 +1,10 @@
 import { Button, Input } from 'antd';
 import Product from '@configs/models/product.model';
 import { Minus, Plus } from 'react-feather';
-import React from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useCart } from '@providers/CartProvider';
 import { PromotionPercent } from '@configs/models/promotion.model';
+import { useAppConfirmDialog } from '@providers/AppConfirmDialogProvider';
 
 const putPromotionIntoNonPromotionProduct = (
   product: Product,
@@ -29,9 +30,51 @@ function AddToCartButton({
 }) {
   const { addToCart, cartProducts, removeFromCart } = useCart();
 
-  const productIncart = cartProducts.find(
-    (cartProduct) => cartProduct.product.key === product.key
+  const [quantity, setQuantity] = useState(0);
+  const lastestPositiveQuantityRef = useRef<number>();
+  const { setConfirmData } = useAppConfirmDialog();
+
+  const productIncart = useMemo(
+    () =>
+      cartProducts.find(
+        (cartProduct) => cartProduct.product.key === product.key
+      ),
+    [cartProducts, product.key]
   );
+
+  const updateCartQuantity = (quantity: number) => {
+    setQuantity(quantity);
+
+    if (!quantity) {
+      setConfirmData({
+        title: 'Xóa khỏi giỏ hàng',
+        content: `Sản phẩm ${
+          product.detail?.displayName || product.name
+        } sẽ được loại bỏ khỏi giỏ hàng của bạn?`,
+        onOk: () => {
+          removeFromCart(product, {
+            isShowConfirm: false,
+          });
+        },
+        onCancel: () => {
+          setQuantity(lastestPositiveQuantityRef.current || 0);
+        },
+      });
+    } else {
+      lastestPositiveQuantityRef.current = quantity;
+      addToCart({
+        product: putPromotionIntoNonPromotionProduct(product, promotionPercent),
+        quantity,
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (productIncart?.quantity) {
+      setQuantity(productIncart.quantity);
+      lastestPositiveQuantityRef.current = productIncart.quantity;
+    }
+  }, [productIncart?.quantity]);
 
   return (
     <div onClick={(e) => e.preventDefault()} className="w-full">
@@ -41,37 +84,29 @@ function AddToCartButton({
             <Button
               className="min-w-[32px] rounded-lg border-none bg-green-50 disabled:bg-gray-50"
               icon={<Minus size={20} className="text-primary" />}
-              disabled={productIncart.quantity <= 1}
+              disabled={productIncart.quantity <= 0}
               onClick={(e) => {
                 e.preventDefault();
-                if ((productIncart?.quantity || 0) > 1) {
-                  addToCart({
-                    product: putPromotionIntoNonPromotionProduct(
-                      product,
-                      promotionPercent
-                    ),
-                    quantity: (productIncart?.quantity || 1) - 1,
-                  });
+                if ((productIncart?.quantity || 0) > 0) {
+                  updateCartQuantity((productIncart?.quantity || 1) - 1);
                 }
               }}
             />
             <Input
               className="border-none text-center font-semibold outline-none focus:shadow-none focus:outline-none"
-              value={productIncart.quantity || ''}
+              value={quantity || ''}
               onChange={(e) => {
-                e.preventDefault;
-                addToCart({
-                  product: putPromotionIntoNonPromotionProduct(
-                    product,
-                    promotionPercent
-                  ),
-                  quantity: +e.target.value,
-                });
+                e.preventDefault();
+                const value = +e.target.value;
+                setQuantity(value);
+
+                // hold the latest positive value
+                if (value > 0) {
+                  lastestPositiveQuantityRef.current = value;
+                }
               }}
               onBlur={(e) => {
-                if (!e.target.value) {
-                  removeFromCart(product);
-                }
+                updateCartQuantity(+e.target.value);
               }}
             />
             <Button
@@ -79,14 +114,8 @@ function AddToCartButton({
               disabled={productIncart.quantity >= 1000}
               icon={<Plus size={20} className="text-primary" />}
               onClick={(e) => {
-                e.preventDefault;
-                addToCart({
-                  product: putPromotionIntoNonPromotionProduct(
-                    product,
-                    promotionPercent
-                  ),
-                  quantity: (productIncart?.quantity || 1) + 1,
-                });
+                e.preventDefault();
+                updateCartQuantity((productIncart?.quantity || 0) + 1);
               }}
             />
           </Input.Group>
