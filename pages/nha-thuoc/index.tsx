c259e98d155f35981f80e480194383bfe66cbcc5
letuthptnguyendu <letuthptnguyendueduvn@gmail.com>
2023-06-08 13:48:07 +0700

    update: drugstore item

--- pages/nha-thuoc/index.tsx
+++ pages/nha-thuoc/index.tsx
@@ -28,7 +28,7 @@ const DrugstorePage: NextPageWithLayout<{
         Hệ thống các chi nhánh nhà thuốc Phước Thiện
       </Typography.Title>
 
-      <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-4">
+      <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3 lg:gap-3">
         {drugstores?.map((drugStore) => (
           <DrugstoreItem
             variant="card"
