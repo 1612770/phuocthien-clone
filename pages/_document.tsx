1467bf3874c81ad21e2d372f837534c6bf30918e
Tư Lê <112106266+1612770@users.noreply.github.com>
2024-07-22 09:58:03 +0700

    [uat] update/self-hosted-inter-font (#68)
    * update: default loading and mock remove
    
    * update: seo host inter font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,10 +13,6 @@ class MyDocument extends Document {
     return (
       <Html lang="vi">
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          ></link>
           <meta name="author" content="nhathuocphuocthien.com" />
           <link
             rel="icon"
